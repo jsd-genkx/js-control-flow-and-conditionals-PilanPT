3a78f7e61e7870547bfbdb467334b1f9eebb989f
PilanPT <pilan.pt@gmail.com>
2025-07-15 13:15:43 +0700

    finsihed task03

--- task03.js
+++ task03.js
@@ -5,4 +5,8 @@
 // 6–10 chars → "Moderate"
 // 11+ chars → "Strong"
 
-const password = "pass123";
+const password = "pass1234567890";
+if (password.length<6) {console.log("Too short")}
+else if (password.length<11) {console.log("Moderate")}
+else {console.log("Strong")}
+
