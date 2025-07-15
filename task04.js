e53ad74a03e55965b411bf55d2ae753e4e6f9ea1
PilanPT <pilan.pt@gmail.com>
2025-07-15 13:34:31 +0700

    Finished task04 with Bonus

--- task04.js
+++ task04.js
@@ -8,4 +8,23 @@
 
 // TODO BONUS: Rewrite with if
 
-const num = 15;
+const num = 26;
+
+if ((num%3===0)&&(num%5===0)) { console.log("FizzBuzz");
+    
+} else if(num%3===0){ console.log("Fizz")
+    
+} else if(num%5===0) console.log("Buzz")
+
+else console.log("Not divisible by 3 or 5")
+
+
+console.log("****BONUS****")
+
+if(num%15===0) {console.log("FizzBuzz");
+    
+}
+
+if((num%3===0)&&(num%5!==0)){console.log("Fizz")}
+if((num%5===0)&&(num%3!==0)){console.log("Buzz")}
+if((num%5!==0)&&(num%3!==0)){console.log("Not divisible by 3 or 5")}
